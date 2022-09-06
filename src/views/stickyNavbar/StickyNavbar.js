efe37bbcd0f1ee4147c694f7a22dcbd187390a13
a.h.jafari <amirjaffari300245@gmail.com>
2022-09-06 11:18:48 +0430

    handel page up button for page

--- src/views/stickyNavbar/StickyNavbar.js
+++ src/views/stickyNavbar/StickyNavbar.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { VerticalAlignTop } from '@material-ui/icons';
 
-const StickyNavbar = () => {
+const StickyNavbar = (props) => {
 
     const [scrollTop, setScrollTop] = useState(false);
+    const [isShowPageUpButton, setIsShowPageUpButton] = useState(false);
 
     const scrollHandler = () => {
         if (window.scrollY > 0)
@@ -10,19 +12,36 @@ const StickyNavbar = () => {
 
         if (window.scrollY === 0)
             setScrollTop(false);
+
+        if (window.scrollY > 400)
+            setIsShowPageUpButton(true);
+
+        if (window.scrollY < 400)
+            setIsShowPageUpButton(false);
     }
 
     window.addEventListener('scroll', scrollHandler);
 
+    const pageUpHandler = () => {
+        console.log("RUNING");
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
     return (
         <div className='flex flex-col justify-start items-start'>
 
-            <ul className={`flex justify-center p-0 m-0 border-b-2 items-center w-full ${!!scrollTop ? ' sticky top-0 bg-gray-600 text-white' : 'bg-gray-400 text-black'}`}>
+            <ul className={`flex justify-center p-0 m-0 border-b-2 items-center w-full 
+            ${!!scrollTop ? ' sticky top-0 bg-gray-600 text-white' : 'bg-gray-400 text-black'}`}>
                 <li className='px-16 py-5 text-lg hover:bg-gray-500 hover:font-bold'>Home</li>
                 <li className='px-16 py-5 text-lg hover:bg-gray-500 hover:font-bold'>Shop</li>
                 <li className='px-16 py-5 text-lg hover:bg-gray-500 hover:font-bold'>About</li>
                 <li className='px-16 py-5 text-lg hover:bg-gray-500 hover:font-bold'>Contact</li>
             </ul>
+            {isShowPageUpButton &&
+                <div onClick={pageUpHandler} className='flex-center fixed bottom-4 right-4 cursor-pointer bg-slate-500 opacity-40 rounded-3xl text-5xl h-12 w-12 px-5'>
+                    <VerticalAlignTop fontSize='inherit' className='text-white' />
+                </div>
+            }
 
             <div className='h-screen w-full flex-center bg-purple-400 text-black'>
                 <h2>Start Scroll :)</h2>
@@ -67,6 +86,8 @@ const StickyNavbar = () => {
             <div className='h-screen w-full flex-center bg-red-400 text-black'>
                 <h2>it's finish</h2>
             </div>
+
+
 
         </div>
     );
