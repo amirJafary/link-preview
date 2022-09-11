249927a378553da8e917723d750796addbdf1154
a.h.jafari <amirjaffari300245@gmail.com>
2022-09-11 18:11:21 +0430

    improve range slider component

--- src/views/components/RangeSlider.js
+++ src/views/components/RangeSlider.js
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import {Slider,Box} from '@material-ui/core';
+import { Slider, Box } from '@material-ui/core';
 
 export default function RangeSlider(props) {
 
     return (
         <div className='h-6'>
-            <div className='mb-3'>filter your price</div>
-            <Slider
-                value={props.value}
-                min={props?.min}
-                max={props?.max}
-                onChange={props.rangeSliderChanged}
-                valueLabelDisplay="auto"
-                getAriaValueText={props.valueText}
-            />
+            <Box sx={props.style}>
+                <Slider
+                    value={props.value}
+                    min={props?.min}
+                    max={props?.max}
+                    onChange={props.rangeSliderChanged}
+                    valueLabelDisplay={props.labelStatus}
+                    getAriaValueText={props.valueText}
+                />
+            </Box>
         </div>
     );
 }
