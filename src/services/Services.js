19fe69ccf1b0596b504e0f55aef9e9d416b10417
a.h.jafari <amirjaffari300245@gmail.com>
2022-10-17 10:43:28 +0330

    add some services

--- src/services/Services.js
+++ src/services/Services.js
@@ -29,8 +29,74 @@ class Services {
             })
             .catch(error => {
                 callBack({ error: error.message, hasError: true })
-            }
-            )
+            })
+    }
+
+    static getRandomUser = (callBack) => {
+        let url = 'https://randomuser.me/api/'
+
+        axios.get(url)
+            .then(res => {
+                callBack({ hasError: false, data: res.data.results[0] });
+            })
+            .catch(error => {
+                callBack({ error: error.message, hasError: true })
+            })
+    }
+
+    static addNewUserToFirebaseDB = (params, callBack) => {
+        let baseUrl = 'https://test-68223-default-rtdb.firebaseio.com/users.json'
+        axios.post(baseUrl, params)
+            .then(res => {
+                callBack({ hasError: false, res })
+            })
+            .catch(error => {
+                callBack({ error: error.message, hasError: true })
+            })
+    }
+
+    static getUseraFromFirebaseDB = (callBack) => {
+        let baseUrl = 'https://test-68223-default-rtdb.firebaseio.com/users.json'
+        axios.get(baseUrl)
+            .then(res => {
+                callBack({ hasError: false, data: Object.entries(res.data) })
+            })
+            .catch(error => {
+                callBack({ error: error.message, hasError: true })
+            })
+    }
+
+    static deleteUserFromFirebaseDB = (id, callBack) => {
+        let baseUrl = `https://test-68223-default-rtdb.firebaseio.com/users/${id}.json`
+        axios.get(baseUrl)
+            .then(res => {
+                callBack({ hasError: false, res })
+            })
+            .catch(error => {
+                callBack({ error: error.message, hasError: true })
+            })
+    }
+
+    static editUserFromFirebaseDB = (id, callBack) => {
+        let baseUrl = `https://test-68223-default-rtdb.firebaseio.com/users/${id}.json`
+        axios.put(baseUrl)
+            .then(res => {
+                callBack({ hasError: false, res })
+            })
+            .catch(error => {
+                callBack({ error: error.message, hasError: true })
+            })
+    }
+
+    static dictionary = (word, callBack) => {
+        let baseUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        axios.get(baseUrl)
+            .then(res => {
+                callBack({ hasError: false, data:res.data[0] })
+            })
+            .catch(error => {
+                callBack({ hasError: true, error: error.message })
+            })
     }
 
 }
